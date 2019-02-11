6c0bd3f9ab1db2300918e13603726cdc39a83e89
YanzuWuu <43078590+YanzuWuu@users.noreply.github.com>
2019-02-11 08:44:50 -0500

    Update AI_module.py
    add module source

--- AI_module.py
+++ AI_module.py
@@ -1,3 +1,4 @@
+#https://github.com/leonshen95/EC500-Modular-design-2.4/blob/master/input_api.py
 # Input: ID(from main function perhaps), infoDB(from Database function)
 # output: Three predicted parameters, three Alert signals(Type:Boolean
 
