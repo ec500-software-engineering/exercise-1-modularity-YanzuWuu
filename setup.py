066b661fcf4b590f6f8ab7530cecbe0cc3635a18
YanzuWuu <43078590+YanzuWuu@users.noreply.github.com>
2019-02-13 22:12:57 -0500

    Upload setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Patients detection',
+    version='0.0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='Yuncheng Zhu',
+    author_email='yczhu1@bu.edu',
+    description='Connect multiple modules to one product, using threads to achieve Asynchronous '
+)
